9929fdcc9a26b13dbc77cf4ecbebb8d1e7d0b5e9
Lars <lars.kolankiewicz@gmail.com>
2023-09-15 18:02:57 -0400

    typo

--- src/components/SearchMovies/index.jsx
+++ src/components/SearchMovies/index.jsx
@@ -12,7 +12,7 @@ export default function SearchMovies () {
     const search = async (event) => {
         event.preventDefault()
 
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_API}&language=en-US&query=${query}&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;
         try {
             const res = await fetch(url)
             const data = await res.json()
