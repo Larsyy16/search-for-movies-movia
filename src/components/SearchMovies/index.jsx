28462994a01099fb805e426acf52f1d2744fad9f
Lars <lars.kolankiewicz@gmail.com>
2023-09-15 12:42:55 -0400

    MovieDetail and SearchMovies updated

--- src/components/SearchMovies/index.jsx
+++ src/components/SearchMovies/index.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import './style.css'
+import MovieDetail from '../MovieDetail'
 
 export default function SearchMovies () {
 
@@ -23,21 +24,7 @@ export default function SearchMovies () {
         }
     } 
 
-    const movieElements = movies.filter(movie=>movie.poster_path).map((movie) => (
-        <div className="card" key={movie.id}>
-            <img className="card--image"
-            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
-            alt={movie.title + ' poster'}
-            />
-            <div className="card--content">
-                <h3 className="card--title">{movie.title}</h3>
-                <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                <p><small>RATING: {movie.vote_average}</small></p>
-                <p className="card--desc">{movie.overview}</p>
-                </div>
-    
-    </div>
-    ) )
+
 
     
     return (
@@ -50,8 +37,7 @@ export default function SearchMovies () {
             <button className='button' type='submit'> Submit</button>
         </form>
         <div> 
-            {movieElements}
-
+        <MovieDetail movies={movies} />
         </div>
     </>
 
