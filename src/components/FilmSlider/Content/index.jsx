b576cccff629ff01298aab2330225e2210f93204
Lars <lars.kolankiewicz@gmail.com>
2023-10-29 21:14:48 -0400

    homepage ui updates

--- src/components/FilmSlider/Content/index.jsx
+++ src/components/FilmSlider/Content/index.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import IconCross from "../../../assets/IconCross";
 import "./style.scss";
 
-const Content = ({ movie, onClose }) => (
+
+function Content ({ movie, onClose }) { return (
   <div className="content">
     <div className="content__background">
       <div className="content__background__shadow" />
@@ -13,8 +14,18 @@ const Content = ({ movie, onClose }) => (
         }}
       />
     </div>
+
+    <div
+      className="content__background__mobileImage"
+      style={{
+        backgroundImage:`linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)),url(https://image.tmdb.org/t/p/original${movie.image})`,
+      }}/>
+
     <div className="content__area">
-      <div className="content__area__container">
+      <div className="content__area__container"   style={{
+    backgroundImage: 'linear-gradient(to bottom, #000, transparent)',
+    padding: '1rem'
+  }}>
         <div className="content__title">{movie.title}</div>
         <div className="content__description">{movie.overview}</div>
       </div>
@@ -24,5 +35,8 @@ const Content = ({ movie, onClose }) => (
     </div>
   </div>
 );
-
+      }
 export default Content;
+
+
+//i need to chAnge to album image over backdrop image for mobile.
