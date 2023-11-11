7fe5f376de496f4c9a9efe5557d6cc28668faa78
Lars <lars.kolankiewicz@gmail.com>
2023-11-11 15:05:04 -0500

    fix basename

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/search-for-movies/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/search-for-movies-movia/" : "/",
 });
